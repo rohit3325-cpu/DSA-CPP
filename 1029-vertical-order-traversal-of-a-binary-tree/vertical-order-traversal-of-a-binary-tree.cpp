c9852888d512ebfe1660d46361c5a81c6537cac9
Rohit Gupta <r2614476@gmail.com>
2025-10-09 23:24:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (57.23%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -35,7 +35,7 @@ public:
         }
 
         vector<vector<int>> ans;
-        for (auto& p : nodes) {
+        for (auto p : nodes) {
             vector<int> col;
             for (auto& q : p.second) {
                 col.insert(col.end(), q.second.begin(), q.second.end());
