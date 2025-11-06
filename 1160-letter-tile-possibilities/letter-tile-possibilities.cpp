f4f8a89160d530325ba5d22757fb798a7f75583b
Rohit Gupta <r2614476@gmail.com>
2025-11-06 23:03:05 +0530

    Time: 36 ms (27.65%) | Memory: 17 MB (31.55%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int n;
+
+    void solve(string &tiles, vector<bool> &used, unordered_set<string> &result, string &curr) {
+        if (!curr.empty()) {
+            result.insert(curr);
+        }
+
+        for (int i = 0; i < n; i++) {  
+            if (used[i]) 
+                continue;
+
+            used[i] = true;
+            curr.push_back(tiles[i]);
+
+            solve(tiles, used, result, curr); // explore further
+
+            used[i] = false;           // backtrack
+            curr.pop_back();
+        }
+    }
+
+    int numTilePossibilities(string tiles) {
+        n = tiles.length();
+        vector<bool> used(n, false);
+        unordered_set<string> result;
+        string curr = "";
+
+        solve(tiles, used, result, curr);
+        return result.size();
+    }
+};
