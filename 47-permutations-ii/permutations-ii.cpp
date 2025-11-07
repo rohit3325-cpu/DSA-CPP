dac0c763eb658a234039bf2ec119b28a8c840c5a
Rohit Gupta <r2614476@gmail.com>
2025-11-07 10:51:00 +0530

    Time: 3 ms (66.01%) | Memory: 11.4 MB (91.20%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -5,9 +5,10 @@ public:
             result.push_back(curr);
         }
         for (int i = 0; i < nums.size(); i++) {  
-            if ( used[i]) 
+            if(used[i]){
                 continue;
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+        }
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i-1] ) continue;
             used[i] = true;
             curr.push_back(nums[i]);
 
