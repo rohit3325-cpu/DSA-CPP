f804435745e9baf02873cd6128a3f473ab09be0d
Rohit Gupta <r2614476@gmail.com>
2025-11-30 11:56:01 +0530

    Time: 14 ms (10.97%) | Memory: 38.9 MB (21.46%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -13,12 +13,9 @@ class Solution {
 public:
     unordered_set<int> set;
     bool findTarget(TreeNode* root, int k) {
-        
         if(root==NULL) return false;
-
         if(set.count(k-root->val)) return true;
         set.insert(root->val);
-
         return findTarget(root->left,k)||findTarget(root->right,k);
     }
 };
