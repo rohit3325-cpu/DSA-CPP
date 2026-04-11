0704a3a6b6dcc936e687de790f9c02763c271ff8
Rohit Gupta <r2614476@gmail.com>
2026-04-11 16:30:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (49.40%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int min=0;
         int max=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i] == '('){
+        int min=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
                 min++;
-                max++;
-            }
-            else if(s[i] == ')'){
+                max++;;
+            }else{if(s[i]==')'){
                 min--;
                 max--;
+            }else{
+                min--;
+                max++;
             }
-            else{
-                min=min-1;
-                max=max+1;
+            
             }
-            if(min<0) min  =0;
+            if(min <0) min=0;
             if(max<0) return false;
         }
-        return(min ==0);
+        return (min ==0);
     }
 };
