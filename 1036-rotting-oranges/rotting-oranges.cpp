958b642426d8dda52dda10659b03908f263ccc67
Rohit Gupta <r2614476@gmail.com>
2026-04-18 16:26:21 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (85.95%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -8,45 +8,43 @@ public:
         int cnt=0;
         int time=0;
         queue<pair<int,int>> q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]!=0){
-                    tot++;
-                }
-                if(grid[i][j]==2){
-                    q.push({i,j});
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]==2){
+                q.push({i,j});
+            }else{
+                if(grid[i][j]==1){
+                    cnt +=1;
                 }
             }
         }
-        //for all direction
-        int dx[4]={0,0,1,-1};
-        int dy[4]={1,-1,0,0};
+       }
 
-        while(!q.empty()){
-            int k=q.size();
-            cnt+=k;
-            while(k--){
-                int x=q.front().first;
-                int y=q.front().second;
-                q.pop();
+       //for all direction
+       int dx[4]={1,-1,0,0};
+       int dy[4]={0,0,1,-1};
 
-                for(int i=0;i<4;i++){
-                    int nx=x+dx[i];
-                    int ny=y+dy[i];
+       while(!q.empty()){
+        int k=q.size();
+        bool rotten = false;
+        while(k--){
+          auto[x,y] = q.front();
+          q.pop();
 
-                    if(nx<0 || ny<0 || nx>=m || ny>=n || grid[nx][ny]!=1 ){
-                        continue;
-                    }
-                        grid[nx][ny] = 2;
-                        q.push({nx,ny});  
-                    
-
-                }
-            }
-            if(!q.empty()){
-                time++;
-            }
-        }
-        return tot == cnt ? time : -1;
+          for(int i=0;i<4;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];;
+          if(nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == 1){
+            grid[nx][ny] =2;
+            q.push({nx,ny});
+            cnt--;
+            rotten = true;
+          }
+            
+          }
+          }
+          if(rotten) time++;
+       }
+       return cnt ==0 ? time:-1;
     }      
 };
