9b3d9047e84223fee97b20a5550fb449b7654143
Rohit Gupta <r2614476@gmail.com>
2026-04-19 22:46:16 +0530

    Time: 23 ms (84.16%) | Memory: 18.3 MB (38.26%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int count=0;
-        int dx[4]={0,0,-1,1};
-        int dy[4]={-1,1,0,0};
-        
+        int m= grid.size();
+        int n= grid[0].size();
+
         queue<pair<int,int>> q;
-         
-         for(int i=0;i<m;i++){
+        int count=0;
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-            if(grid[i][j]=='1'){
-                count+=1;
+                if(grid[i][j]=='1'){
                 q.push({i,j});
                 grid[i][j]='0';
-                while(!q.empty()){
-                    int x=q.front().first;
-                    int y=q.front().second;
-                    q.pop();
-                    //four direction
-                    for(int i=0;i<4;i++){
-                        int nx=dx[i]+x;
-                        int ny=dy[i]+y;
-                        if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny]=='1'){
-                            q.push({nx,ny});
-                            grid[nx][ny]='0';
-                        }
-                    }
+                count++;
+                }
+        while(!q.empty()){
+            auto[x,y] = q.front();
+            q.pop();
+
+            int dx[4]={0,0,1,-1};
+            int dy[4]={1,-1,0,0};
+
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+
+                if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny]=='1'){
+                    q.push({nx,ny});
+                    grid[nx][ny]=0;
                 }
             }
+        }
             }
-         }
-         
-        
+        }
         return count;
     }
 };
