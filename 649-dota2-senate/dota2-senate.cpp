915e613fb381390eb51ecb6ab4eaede0c3611efb
Rohit Gupta <r2614476@gmail.com>
2026-07-22 20:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (8.89%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> rad,dir;
+        int n=senate.length();
+        for(int i=0;i<senate.length();i++){
+            if(senate[i] == 'R'){
+                rad.push(i);
+            }else{
+                dir.push(i);
+            }
+        }
+
+        while(!rad.empty() && !dir.empty()){
+            if(rad.front()<dir.front()){
+                rad.push(n++);
+            }
+            else{
+                dir.push(n++);
+            }
+            rad.pop();
+            dir.pop();
+        }
+        return (rad.empty()?"Dire":"Radiant");
+    }
+};
