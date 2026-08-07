bda9c55be6b28069e856bc30ab969d89e9a70def
Rohit Gupta <r2614476@gmail.com>
2026-08-07 20:10:36 +0530

    Time: 27 ms (87.93%) | Memory: 34.2 MB (55.17%) - LeetSync

--- 3635-smallest-divisible-digit-product-ii/smallest-divisible-digit-product-ii.cpp
+++ 3635-smallest-divisible-digit-product-ii/smallest-divisible-digit-product-ii.cpp
@@ -1,133 +1,180 @@
+
+
 class Solution {
 public:
+    vector<int> primes = {2, 3, 5, 7};
+    int maxPrime = 7;
+
     string smallestNumber(string num, long long t) {
-        int req2 = 0, req3 = 0, req5 = 0, req7 = 0;
-        long long temp = t;
-        while (temp % 2 == 0) { temp /= 2; req2++; }
-        while (temp % 3 == 0) { temp /= 3; req3++; }
-        while (temp % 5 == 0) { temp /= 5; req5++; }
-        while (temp % 7 == 0) { temp /= 7; req7++; }
-        if (temp > 1) return "-1";
 
-        int dp[60][40];
-        for (int i = 0; i < 60; ++i) {
-            for (int j = 0; j < 40; ++j) {
-                dp[i][j] = 1e9;
-            }
-        }
-        dp[0][0] = 0;
-        
-        int trans[6][2] = {{1, 0}, {0, 1}, {2, 0}, {1, 1}, {3, 0}, {0, 2}};
-        for (int i = 0; i < 60; ++i) {
-            for (int j = 0; j < 40; ++j) {
-                if (dp[i][j] == 1e9) continue;
-                for (auto& tr : trans) {
-                    int ni = min(59, i + tr[0]);
-                    int nj = min(39, j + tr[1]);
-                    dp[ni][nj] = min(dp[ni][nj], dp[i][j] + 1);
-                }
-            }
-        }
-        for (int i = 59; i >= 0; --i) {
-            for (int j = 39; j >= 0; --j) {
-                if (i < 59) dp[i][j] = min(dp[i][j], dp[i + 1][j]);
-                if (j < 39) dp[i][j] = min(dp[i][j], dp[i][j + 1]);
+        vector<int> primeCount(maxPrime + 1, 0);
+
+        int numLength = num.length();
+        int firstZeroIndexFromLeft = 0;
+
+        for (int prime : primes) {
+            while (t % prime == 0) {
+                t /= prime;
+                primeCount[prime]++;
             }
         }
 
-        int F2[] = {0, 0, 1, 0, 2, 0, 1, 0, 3, 0};
-        int F3[] = {0, 0, 0, 1, 0, 0, 1, 0, 0, 2};
-        int F5[] = {0, 0, 0, 0, 0, 1, 0, 0, 0, 0};
-        int F7[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0};
+        if (t != 1)
+            return "-1";
 
-        int n = num.length();
-        bool has_zero = false;
-        int first_zero = n;
-        for (int i = 0; i < n; ++i) {
-            if (num[i] == '0') {
-                has_zero = true;
-                first_zero = i;
+        int minLength = getMinLength(primeCount);
+
+        if (numLength < minLength) {
+            string result(minLength, '0');
+            return buildSuffix(primeCount, minLength, result);
+        }
+
+        string result(numLength + 1, '0');
+
+        int i = 0;
+        while (firstZeroIndexFromLeft < numLength) {
+
+            result[++i] = num[firstZeroIndexFromLeft];
+
+            if (result[i] == '0')
                 break;
-            }
+
+            logNum(primeCount, result[i], -1);
+            firstZeroIndexFromLeft++;
         }
 
-        if (!has_zero) {
-            int r2 = req2, r3 = req3, r5 = req5, r7 = req7;
-            for (char c : num) {
-                int d = c - '0';
-                r2 = max(0, r2 - F2[d]);
-                r3 = max(0, r3 - F3[d]);
-                r5 = max(0, r5 - F5[d]);
-                r7 = max(0, r7 - F7[d]);
-            }
-            if (r2 == 0 && r3 == 0 && r5 == 0 && r7 == 0) return num;
+        if (getMinLength(primeCount) == 0) {
+
+            if (firstZeroIndexFromLeft == numLength)
+                return num;
+
+            firstZeroIndexFromLeft++;
+
+            for (int j = firstZeroIndexFromLeft; j < result.size(); j++)
+                result[j] = '1';
+
+            return result.substr(1, numLength);
         }
 
-        int limit = min(n - 1, first_zero);
-        int p2 = 0, p3 = 0, p5 = 0, p7 = 0;
-        for (int i = 0; i < limit; ++i) {
-            int d = num[i] - '0';
-            p2 += F2[d];
-            p3 += F3[d];
-            p5 += F5[d];
-            p7 += F7[d];
-        }
+        int last = numLength - 1;
 
-        for (int i = limit; i >= 0; --i) {
-            int start_d = (num[i] - '0') + 1;
-            for (int d = start_d; d <= 9; ++d) {
-                int n2 = max(0, req2 - p2 - F2[d]);
-                int n3 = max(0, req3 - p3 - F3[d]);
-                int n5 = max(0, req5 - p5 - F5[d]);
-                int n7 = max(0, req7 - p7 - F7[d]);
-                int L = n - 1 - i;
-                
-                if (n7 + n5 + dp[n2][n3] <= L) {
-                    string ans = num.substr(0, i) + to_string(d);
-                    int rem2 = n2, rem3 = n3, rem5 = n5, rem7 = n7;
-                    for (int pos = 0; pos < L; ++pos) {
-                        for (int x = 1; x <= 9; ++x) {
-                            int nn2 = max(0, rem2 - F2[x]);
-                            int nn3 = max(0, rem3 - F3[x]);
-                            int nn5 = max(0, rem5 - F5[x]);
-                            int nn7 = max(0, rem7 - F7[x]);
-                            if (nn7 + nn5 + dp[nn2][nn3] <= L - 1 - pos) {
-                                ans += to_string(x);
-                                rem2 = nn2; rem3 = nn3; rem5 = nn5; rem7 = nn7;
-                                break;
-                            }
-                        }
-                    }
-                    return ans;
+        for (int end = min(firstZeroIndexFromLeft, last); end >= 0; end--) {
+
+            logNum(primeCount, result[end + 1], 1);
+
+            while (++result[end + 1] <= '9') {
+
+                logNum(primeCount, result[end + 1], -1);
+
+                if (getMinLength(primeCount) <= last - end) {
+                    return buildSuffix(primeCount, last - end, result);
                 }
-            }
-            if (i > 0) {
-                int d = num[i - 1] - '0';
-                p2 -= F2[d];
-                p3 -= F3[d];
-                p5 -= F5[d];
-                p7 -= F7[d];
+
+                logNum(primeCount, result[end + 1], 1);
             }
         }
 
-        int min_len_needed = req7 + req5 + dp[req2][req3];
-        int M = max(n + 1, min_len_needed);
-        string ans = "";
-        int rem2 = req2, rem3 = req3, rem5 = req5, rem7 = req7;
-        
-        for (int pos = 0; pos < M; ++pos) {
-            for (int x = 1; x <= 9; ++x) {
-                int nn2 = max(0, rem2 - F2[x]);
-                int nn3 = max(0, rem3 - F3[x]);
-                int nn5 = max(0, rem5 - F5[x]);
-                int nn7 = max(0, rem7 - F7[x]);
-                if (nn7 + nn5 + dp[nn2][nn3] <= M - 1 - pos) {
-                    ans += to_string(x);
-                    rem2 = nn2; rem3 = nn3; rem5 = nn5; rem7 = nn7;
-                    break;
-                }
-            }
+        return buildSuffix(primeCount, result.length(), result);
+    }
+
+private:
+
+    void logNum(vector<int>& primeCount, char num, int value) {
+
+        if (num < '2')
+            return;
+
+        if (num == '9') {
+
+            primeCount[3] += value * 2;
+
+        } else if (num == '4') {
+
+            primeCount[2] += value * 2;
+
+        } else if (num == '8') {
+
+            primeCount[2] += value * 3;
+
+        } else if (num == '6') {
+
+            primeCount[2] += value;
+            primeCount[3] += value;
+
+        } else {
+
+            primeCount[num - '0'] += value;
         }
-        return ans;
+    }
+
+    string buildSuffix(vector<int> primeCount,
+                       int targetLength,
+                       string result) {
+
+        int index = result.length();
+
+        while (primeCount[3] > 1) {
+            primeCount[3] -= 2;
+            result[--index] = '9';
+        }
+
+        while (primeCount[2] > 2) {
+            primeCount[2] -= 3;
+            result[--index] = '8';
+        }
+
+        while (primeCount[7]-- > 0)
+            result[--index] = '7';
+
+        if (primeCount[2] > 0 && primeCount[3] > 0) {
+
+            result[--index] = '6';
+
+            primeCount[2]--;
+            primeCount[3]--;
+        }
+
+        while (primeCount[5]-- > 0)
+            result[--index] = '5';
+
+        while (primeCount[2] > 1) {
+
+            primeCount[2] -= 2;
+            result[--index] = '4';
+        }
+
+        while (primeCount[3] > 0) {
+
+            primeCount[3]--;
+            result[--index] = '3';
+        }
+
+        while (primeCount[2] > 0) {
+
+            primeCount[2]--;
+            result[--index] = '2';
+        }
+
+        while (index + targetLength != result.length())
+            result[--index] = '1';
+
+        if (targetLength == result.length())
+            return result;
+
+        return result.substr(1);
+    }
+
+    int getMinLength(vector<int>& primeCount) {
+
+        int count2 = max(0, primeCount[2]);
+        int count3 = max(0, primeCount[3]);
+
+        int count23 = (count3 & 1) + (count2 % 3);
+
+        return (count3 / 2)
+                + (count2 / 3)
+                + max(0, primeCount[7])
+                + max(0, primeCount[5])
+                + (count23 == 3 ? 2 : (count23 > 0 ? 1 : 0));
     }
 };
